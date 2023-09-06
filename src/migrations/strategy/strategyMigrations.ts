c246e994fbdbacd10e2e7e003bfb9b80c78d456d
Mitul Desai <mitul.desai@gmetri.com>
2023-09-06 09:11:51 +0000

    removed migration

--- src/migrations/strategy/strategyMigrations.ts
+++ src/migrations/strategy/strategyMigrations.ts
@@ -1,10 +1,8 @@
 import { IOrder } from "../IOrder";
 import m001_002 from "./strategy-migration-commands/m001_002_initial_migration";
-import m002_003 from "./strategy-migration-commands/m002_003_adding_character";
 
 export const strategyMigrationTree: { [key: number]: IOrder } = {
-  [1]: m001_002,
-  [2]: m002_003,
+  [1]: m001_002
 };
 
 export const getHighestStrategyVersion = (): number => {

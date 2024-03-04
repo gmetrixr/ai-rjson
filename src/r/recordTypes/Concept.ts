3e943c235c0b2d956d15429ef20328876480b6fc
Amit Rajput <amitrajput123.ar@gmail.com>
2024-03-04 17:23:07 +0530

    Adding relevance score to concepts

--- src/r/recordTypes/Concept.ts
+++ src/r/recordTypes/Concept.ts
@@ -1,9 +1,11 @@
 export enum ConceptProperty {
   title = "title",
   content = "content",
+  relevance_score = "relevance_score",
 }
 
 export const conceptPropertyDefaults: Record<ConceptProperty, unknown> = {
   [ConceptProperty.title]: "",
   [ConceptProperty.content]: "",
+  [ConceptProperty.relevance_score]: 0,
 };
